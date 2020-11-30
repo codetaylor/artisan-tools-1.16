804a230d1a283cf0455d8c79ad0fc44321697766
codetaylor <jason@codetaylor.com>
2020-11-30 15:08:07 -0800

    Add all tool material definitions

--- src/main/java/com/codetaylor/mc/artisantools/material/DataCustomMaterialListFactory.java
+++ src/main/java/com/codetaylor/mc/artisantools/material/DataCustomMaterialListFactory.java
@@ -95,6 +95,161 @@ public class DataCustomMaterialListFactory {
         "material.artisantools.netherite"
     ));
 
+    // Flint
+    list.add(new DataCustomMaterial(
+        "flint",
+        1,
+        150,
+        3.8f,
+        1,
+        5,
+        "5F5F5F",
+        true,
+        "item#minecraft:flint",
+        "material.artisantools.flint"
+    ));
+
+    // Bone
+    list.add(new DataCustomMaterial(
+        "bone",
+        1,
+        150,
+        3.8f,
+        1,
+        5,
+        "FFF6C8",
+        false,
+        "tag#forge:bones",
+        "material.artisantools.bone"
+    ));
+
+    // Bronze
+    list.add(new DataCustomMaterial(
+        "bronze",
+        2,
+        500,
+        6,
+        2,
+        15,
+        "E8983F",
+        true,
+        "tag#forge:ingots/bronze",
+        "material.artisantools.bronze"
+    ));
+
+    // Constantan
+    list.add(new DataCustomMaterial(
+        "constantan",
+        2,
+        275,
+        6,
+        1.5f,
+        20,
+        "BD8D46",
+        true,
+        "tag#forge:ingots/constantan",
+        "material.artisantools.constantan"
+    ));
+
+    // Copper
+    list.add(new DataCustomMaterial(
+        "copper",
+        1,
+        175,
+        4,
+        0.75f,
+        6,
+        "FFA131",
+        true,
+        "tag#forge:ingots/copper",
+        "material.artisantools.copper"
+    ));
+
+    // Electrum
+    list.add(new DataCustomMaterial(
+        "electrum",
+        0,
+        100,
+        14,
+        0.5f,
+        30,
+        "FFE947",
+        true,
+        "tag#forge:ingots/electrum",
+        "material.artisantools.electrum"
+    ));
+
+    // Invar
+    list.add(new DataCustomMaterial(
+        "invar",
+        2,
+        450,
+        7,
+        3,
+        16,
+        "8E9A95",
+        true,
+        "tag#forge:ingots/invar",
+        "material.artisantools.invar"
+    ));
+
+    // Lead
+    list.add(new DataCustomMaterial(
+        "lead",
+        1,
+        150,
+        5,
+        1,
+        9,
+        "8A93B1",
+        true,
+        "tag#forge:ingots/lead",
+        "material.artisantools.lead"
+    ));
+
+    // Nickel
+    list.add(new DataCustomMaterial(
+        "nickel",
+        2,
+        300,
+        6.5f,
+        2.5f,
+        18,
+        "A2975D",
+        true,
+        "tag#forge:ingots/nickel",
+        "material.artisantools.nickel"
+    ));
+
+    // Silver
+    list.add(new DataCustomMaterial(
+        "silver",
+        2,
+        200,
+        6,
+        1.5f,
+        20,
+        "7B9DA4",
+        true,
+        "tag#forge:ingots/silver",
+        "material.artisantools.silver"
+    ));
+
+
+    // Tin
+    list.add(new DataCustomMaterial(
+        "tin",
+        1,
+        200,
+        4.5f,
+        1,
+        7,
+        "7C9AB2",
+        true,
+        "tag#forge:ingots/tin",
+        "material.artisantools.tin"
+    ));
+
     return new DataCustomMaterialList(list);
   }
 }
