abd9efcbc8050f80cc8eff92445ca5d1e0159d84
codetaylor <jason@codetaylor.com>
2020-11-30 11:21:23 -0800

    Add comment to generated material list

--- src/main/java/com/codetaylor/mc/artisantools/material/DataCustomMaterialList.java
+++ src/main/java/com/codetaylor/mc/artisantools/material/DataCustomMaterialList.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class DataCustomMaterialList {
 
+  private String[] __comments = {
+      "WARNING: All changes should be made to the file with the name `custom`",
+      "in the title. Changes made to the `generated` file will be overwritten."
+  };
+
   private List<DataCustomMaterial> list;
 
   public DataCustomMaterialList(List<DataCustomMaterial> list) {
