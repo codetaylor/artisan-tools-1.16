99e7cf121a6c79903c0d5894b5ee50ad38ca7860
codetaylor <jason@codetaylor.com>
2020-12-05 12:09:38 -0800

    Cleanup API event

--- src/api/java/com/codetaylor/mc/artisantools/api/CustomToolMaterialRegistrationEvent.java
+++ src/api/java/com/codetaylor/mc/artisantools/api/CustomToolMaterialRegistrationEvent.java
@@ -1,6 +1,5 @@
 package com.codetaylor.mc.artisantools.api;
 
-import com.codetaylor.mc.artisantools.api.CustomToolMaterialRegistrationEntry;
 import net.minecraftforge.eventbus.api.Event;
 
 import java.util.ArrayList;
@@ -9,7 +8,7 @@ import java.util.List;
 public class CustomToolMaterialRegistrationEvent
     extends Event {
 
-  private List<CustomToolMaterialRegistrationEntry> materialList = new ArrayList<>();
+  private final List<CustomToolMaterialRegistrationEntry> materialList = new ArrayList<>();
 
   public List<CustomToolMaterialRegistrationEntry> getMaterialList() {
 
